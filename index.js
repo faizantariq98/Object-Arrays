4f63e3baec217b73c2d176c2eedbd0c071626fc7
Faizan Tariq <faizantariq498@gmail.com>
2022-11-14 09:48:27 +0500

    Reference vs copy Difference

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Nothing is worth more than laughter.");
+var bestSong = Math.random() > 0.5
+    ? "resect1" : "Respect";
+var rocker;
+rocker = 'faizan';
+console.log(rocker);
+rocker = 1;
+console.log(rocker);
---
+++ index.ts
@@ -0,0 +1,12 @@
+console.log("Nothing is worth more than laughter.");
+const bestSong = Math.random() > 0.5 
+  ? "resect1"   : "Respect";
+
+
+  let rocker;
+
+  rocker='faizan';
+  console.log(rocker);
+
+  rocker=1;
+  console.log(rocker);
